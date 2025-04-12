4537edd4be06103ecbe69a8ca931677acb6b8aab
Mikhail <mikhail.kuliaev@gmail.com>
2025-04-12 12:01:32 +0300

    variab

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,57 @@ resource "yandex_vpc_subnet" "subnets" {
   network_id     = yandex_vpc_network.default.id
   v4_cidr_blocks = ["10.0.${count.index}.0/24"]
 }
+
+
+resource "yandex_compute_instance" "master" {
+  name = "mkuliaev-master"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnets[0].id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file(var.public_key_path)}"
+  }
+}
+
+resource "yandex_compute_instance" "worker" {
+  count = 2
+  name  = "mkuliaev-worker-${count.index}"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnets[count.index + 1].id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file(var.public_key_path)}"
+  }
+}
+
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2004-lts"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,53 +1,24 @@
-variable "yc_token" {
-  description = "Yandex Cloud OAuth token"
-  type        = string
-  sensitive   = true
+variable "cloud_id" {
+  description = "Ваш Cloud ID в Yandex.Cloud"
 }
 
-variable "yc_cloud_id" {
-  description = "Yandex Cloud ID"
-  type        = string
-  sensitive   = true
+variable "folder_id" {
+  description = "Ваш Folder ID в Yandex.Cloud"
 }
 
-variable "yc_folder_id" {
-  description = "Yandex Cloud Folder ID"
-  type        = string
-  sensitive   = true
+variable "access_key" {
+  description = "Access key для S3"
 }
 
-variable "yc_zone" {
-  description = "Yandex Cloud zone"
-  type        = string
-  default     = "ru-central1-a"
+variable "secret_key" {
+  description = "Secret key для S3"
 }
 
-variable "public_cidr" {
-  description = "CIDR for public subnet"
-  type        = string
-  default     = "192.168.10.0/24"
+
+variable "ssh_user" {
+  default = "ubuntu"
 }
 
-variable "private_cidr" {
-  description = "CIDR for private subnet"
-  type        = string
-  default     = "192.168.20.0/24"
-}
-
-variable "nat_ip" {
-  description = "NAT instance internal IP"
-  type        = string
-  default     = "192.168.10.254"
-}
-
-variable "nat_image_id" {
-  description = "NAT instance image id"
-  type        = string
-  default     = "fd80mrhj8fl2oe87o4e1"
-}
-
-variable "ubuntu_image_id" {
-  description = "Ubuntu 24 image id"
-  type        = string
-  default     = "fd8ou6hurlbfqmi57ofd"
+variable "public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
 }
