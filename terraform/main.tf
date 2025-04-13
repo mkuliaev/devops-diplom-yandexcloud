910680fbafac7bd5d1e7582417cc81f432316df4
Mikhail <mikhail.kuliaev@gmail.com>
2025-04-13 12:19:17 +0300

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,72 +1,73 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.89.0"
+    }
+  }
+}
+
 provider "yandex" {
-  service_account_key_file = "key.json"
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
   zone      = "ru-central1-a"
 }
 
-resource "yandex_vpc_network" "default" {
-  name = "mkuliaev-net"
+# Сеть и подсети
+resource "yandex_vpc_network" "kuliaev-network" {
+  name = "kuliaev-network"
 }
 
-resource "yandex_vpc_subnet" "subnets" {
-  count = 3
-  name           = "mkuliaev-subnet-${count.index}"
-  zone           = element(["ru-central1-a", "ru-central1-b", "ru-central1-c"], count.index)
-  network_id     = yandex_vpc_network.default.id
-  v4_cidr_blocks = ["10.0.${count.index}.0/24"]
+resource "yandex_vpc_subnet" "public" {
+  name           = "public"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.kuliaev-network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
+resource "yandex_vpc_subnet" "private" {
+  name           = "private"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.kuliaev-network.id
+  v4_cidr_blocks = ["192.168.20.0/24"]
+}
 
-resource "yandex_compute_instance" "master" {
-  name = "mkuliaev-master"
+# Bastion Host
+resource "yandex_compute_instance" "bastion" {
+  name        = "bastion"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a"
 
   resources {
     cores  = 2
-    memory = 4
+    memory = 2
   }
 
   boot_disk {
     initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.id
+      image_id = "fd8vmcue7aajpmeo39kk" # Ubuntu 22.04
     }
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnets[0].id
+    subnet_id = yandex_vpc_subnet.public.id
     nat       = true
   }
 
   metadata = {
-    ssh-keys = "${var.ssh_user}:${file(var.public_key_path)}"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
 }
 
-resource "yandex_compute_instance" "worker" {
-  count = 2
-  name  = "mkuliaev-worker-${count.index}"
+# Security Groups
+resource "yandex_vpc_security_group" "bastion-sg" {
+  name        = "bastion-sg"
+  network_id  = yandex_vpc_network.kuliaev-network.id
 
-  resources {
-    cores  = 2
-    memory = 4
+  ingress {
+    protocol       = "TCP"
+    port           = 22
+    v4_cidr_blocks = ["0.0.0.0/0"]
   }
-
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.id
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnets[count.index + 1].id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "${var.ssh_user}:${file(var.public_key_path)}"
-  }
-}
-
-data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
 }
